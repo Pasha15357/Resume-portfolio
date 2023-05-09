a06f6ae40ee2899ac58388774bff229bb36b8f96
Pasha15357 <119806730+Pasha15357@users.noreply.github.com>
2023-05-09 14:51:58 +0300

    burger menu

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
 
 } else document.body.classList.add('_pc');
 
+
+//Прокрутка при клике
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
 	function onMenuLinkClick(e) { // Исправлено
@@ -14,6 +16,11 @@ if (menuLinks.length > 0) {
 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
 			window.scrollTo({
 				top: gotoBlockValue,
 				behavior: "smooth"
@@ -24,6 +31,16 @@ if (menuLinks.length > 0) {
 	menuLinks.forEach(menuLink => {
 		menuLink.addEventListener("click", onMenuLinkClick); // Исправлено
 	});
+}
 
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
 }
 
