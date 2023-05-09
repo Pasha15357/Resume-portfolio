4833ff135ac2e3346c73f748622421afeeec2a3c
Pasha15357 <119806730+Pasha15357@users.noreply.github.com>
2023-05-09 12:30:07 +0300

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -7,4 +7,23 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
 
 } else document.body.classList.add('_pc');
 
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	function onMenuLinkClick(e) { // Исправлено
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick); // Исправлено
+	});
+
+}
+
